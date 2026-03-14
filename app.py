4bc253eec9388af0c7b79ffd589428d27cf3548c
Data Scientist <89708093+Ramsha121@users.noreply.github.com>
2026-03-14 20:39:48 +0530

    Update title and subtitle in app.py

--- app.py
+++ app.py
@@ -134,8 +134,8 @@ h2 {
 # ---------------------------------------------------
 # TITLE SECTION
 # ---------------------------------------------------
-st.markdown('<p class="big-title">AI ANALYZER</p>', unsafe_allow_html=True)
-st.markdown('<p class="sub-title">Advanced Resume Intelligence</p>', unsafe_allow_html=True)
+st.markdown('<p class="big-title">AI-Powered-Resume Screening & Job Role Matching System</p>', unsafe_allow_html=True)
+st.markdown('<p class="sub-title">Advanced Resume </p>', unsafe_allow_html=True)
 
 # ---------------------------------------------------
 # NLTK DOWNLOAD (SAME LOGIC)
